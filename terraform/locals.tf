c4f6f84252b11930ae18906bd3ac6a9ce3ad95e1
Garrett O'Gorman <garrett.ogorman@hertz.com>
2022-08-29 11:49:34 +0100

    UPdated Trust

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,7 +5,7 @@ locals {
     Source = "https://github.com/thinkbiggerltd/aws-oidc-token"
   }
   allowed_branches_to_assume_roles = [
-    "repo:https://github.com/thinkbiggerltd/aws-oidc-token:ref:refs/heads/main"
+    "repo:thinkbiggerltd/aws-oidc-token:ref:refs/heads/main"
   ]
   #  By default, this is the URL of the repository owner in this case its the one from aws-actions/configure-aws-credentials
   audience = "sts.amazonaws.com"
